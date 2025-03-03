f4bf330739e790220073bb5ca59a0e0b7a5147b6
gabrielbitello <111650584+gabrielbitello@users.noreply.github.com>
2025-03-03 03:26:28 -0300

    0.09.1

--- BackEnd/config/settings.py
+++ BackEnd/config/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'sua_chave_secreta_aqui')
 
 # Ativar ou desativar o modo de depuração
-DEBUG = os.getenv('DJANGO_DEBUG', 'Ture') == 'True'
+DEBUG = os.getenv('DJANGO_DEBUG', 'True') == 'True'
 
 # Hosts permitidos para segurança (Evite 'ALLOWED_HOSTS' = ['*'] em produção)
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'seu_dominio.com']
@@ -24,16 +24,15 @@ INSTALLED_APPS = [
     'livereload',  # Atualização automática do navegador
 ]
 
-
-# Configuração do banco de dados
+# Configuração do banco de dados (comentei porque está desativada)
 #DATABASES = {
 #    'default': {
-#        'ENGINE': 'django.db.backends.mysql',  # Engine do banco
-#        'NAME': os.getenv('DB_NAME', 'mydb'),  # Nome do banco
-#        'USER': os.getenv('DB_USER', 'user'),  # Usuário do banco
-#        'PASSWORD': os.getenv('DB_PASSWORD', 'password'),  # Senha
-#        'HOST': os.getenv('DB_HOST', 'localhost'),  # Host do banco
-#       'PORT': os.getenv('DB_PORT', '5432'),  # Porta do banco
+#        'ENGINE': 'django.db.backends.mysql',
+#        'NAME': os.getenv('DB_NAME', 'mydb'),
+#        'USER': os.getenv('DB_USER', 'user'),
+#        'PASSWORD': os.getenv('DB_PASSWORD', 'password'),
+#        'HOST': os.getenv('DB_HOST', 'localhost'),
+#        'PORT': os.getenv('DB_PORT', '5432'),
 #    }
 #}
 
@@ -44,24 +43,17 @@ app_template_dirs = []
 for app in os.listdir(BASE_DIR / 'apps'):
     app_template_dir = BASE_DIR / 'apps' / app / 'templates'
     if app_template_dir.exists() and app_template_dir.is_dir():
-        # Adiciona a pasta de templates do app à lista
         app_template_dirs.append(str(app_template_dir))
 
-# Arquivos Estáticos
-STATICFILES_DIRS = [
-    BASE_DIR / 'apps' / 'static',  # Diretório global de arquivos estáticos
-]
-
-# Agora, vamos garantir que o Django apenas busque um diretório 'static' em cada app
-for app in os.listdir(BASE_DIR / 'apps'):
-    app_static_dir = BASE_DIR / 'apps' / app / 'static'
-    if app_static_dir.exists() and app_static_dir.is_dir():
-        STATICFILES_DIRS.append(app_static_dir)
-
-print(f"Static: {STATICFILES_DIRS}\n")
-
 STATIC_URL = '/static/'  # URL para acesso aos arquivos estáticos
-STATIC_ROOT = BASE_DIR / 'staticfiles'  # Diretório onde os arquivos estáticos serão armazenados em produção
+
+# Diretório onde os arquivos estáticos serão armazenados em produção
+STATIC_ROOT = str(BASE_DIR / 'staticfiles')
+
+# Diretórios adicionais para arquivos estáticos no desenvolvimento
+STATICFILES_DIRS = [
+    str(BASE_DIR / 'apps' / 'static'),  # Adicionando a pasta static dos apps
+]
 
 # Arquivos de Mídia
 MEDIA_URL = '/media/'  # URL para uploads de arquivos
